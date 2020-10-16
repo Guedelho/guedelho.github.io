c0bdcfd0380b8c688842d81fe485a4c79f7c2a48
Guedelho <mateus.guedelho@gmail.com>
2020-10-15 23:15:40 -0300

    remove pt version, add projects and update fields

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,24 +2,24 @@ jQuery(document).ready(function($) {
 
 
     /*======= Skillset *=======*/
-    
-    
+
+
     $('.level-bar-inner').css('width', '0');
-    
+
     $(window).on('load', function() {
 
         $('.level-bar-inner').each(function() {
-        
-            var itemWidth = $(this).data('level');
-            
+
+            var itemWidth = Math.floor(Math.random() * (100 - 90) + 90);
+
             $(this).animate({
-                width: itemWidth
+                width: itemWidth + "%"
             }, 800);
-            
+
         });
 
     });
-   
-    
+
+
 
 });
